3f41d17611a8444379b8a86ccd2bcbe5047765e5
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-12-08 12:52:58 -0800

    Implemented dropbox syncing.

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,94 @@
 
  var DownloadsView = (function() {
 
+    function saveCursorKey(key) {
+      chrome.storage.sync.set({
+        dropboxDeltaCursor: key
+      });
+    }
+
+    function getCursorKey(callback){
+      chrome.storage.sync.get('dropboxDeltaCursor', function(data) {
+
+        if(data && data.dropboxDeltaCursor) {
+          callback(data.dropboxDeltaCursor);
+        } else {
+          callback(null);
+        }
+
+      });
+    }
+
+    function storeChanges(changes) {
+      chrome.storage.sync.set({
+        dropboxLastChanges: changes
+      });
+    }
+
+    function getStoredLastChanges(callback) {
+      chrome.storage.sync.get('dropboxLastChanges', function(data) {
+
+        if(data && data.dropboxLastChanges) {
+          callback(data.dropboxLastChanges);
+        } else {
+          callback(null);
+        }
+
+      });
+    }
+
+    function getLatestChangesFromDropbox() {
+
+      var dfd = Q.defer();
+
+      chrome.runtime.getBackgroundPage(function(eventPage) {
+
+        getCursorKey(function(key) {
+
+          console.log('dropbox.pullChanges.fetching');
+
+          eventPage.controller.dropboxClient.pullChanges(key, function(err, data) {
+
+            if(err) {
+              console.log('dropbox.pullChanges.error');
+              dfd.reject(err);
+            }
+
+            console.log('dropbox.pullChanges.done');
+
+            if(data.cursorTag) {
+              saveCursorKey(data.cursorTag);
+              window.console.log('dropbox.pullChanges.cursorTag.updated', data.cursorTag);
+              window.console.log('dropbox.pullChanges.changes.number', data.changes.length);
+            }
+
+            if(data.shouldPullAgain) {
+              console.log('dropbox.pullChanges.pullAgain');
+              getLatestChangesFromDropbox().then(function(changes) {
+                dfd.resolve(changes);
+              });
+            }
+
+            if(data.changes && data.changes.length) {
+              console.log('dropbox.pullChanges.storedChanges');
+              storeChanges(data.changes);
+            }
+
+            getStoredLastChanges(function(changes) {
+              console.log('dropbox.pullChanges.done');
+              dfd.resolve(changes);
+            });
+
+          });
+
+        });
+
+      });
+
+      return dfd.promise;
+
+    };
+
     function DownloadsView(root) {
       var _this = this;
       this.root = root;
@@ -18,13 +106,15 @@
         return _this.onOptionsClick(event);
       });
 
-
       chrome.extension.onMessage.addListener(function(message) {
         return _this.onMessage(message);
       });
 
       this.reloadUserInfo();
       this.updateFileList();
+
+      this.dropboxCursor = null;
+
     }
 
     DownloadsView.prototype.reloadUserInfo = function() {
@@ -46,24 +136,24 @@
     };
 
     DownloadsView.prototype.updateFileList = function() {
-      var _this = this;
-      chrome.runtime.getBackgroundPage(function(eventPage) {
 
+      var promise = getLatestChangesFromDropbox();
+
+      $('section').text('Syncig with Dropbox...');
+
+      promise.done(function(changes) {
         var html = [];
-        return eventPage.controller.dropboxClient.pullChanges(function(err, data) {
-
-          var lastChanges = data.changes.slice(0, 3);
-
-          lastChanges.forEach(function(item) {
-            html.push(JSON.stringify(item), '\t');
-          });
-
-          $('section').text(html.join('</br>'));
-
-
+        changes.forEach(function(item) {
+          html.push(JSON.stringify(item), '\t');
         });
+        $('section').text(html.join('</br>'));
+      }.bind(this));
 
-      });
+      promise.fail(function(err) {
+        $('section').text('Something went wrong' + err);
+        window.console.log('err', err);
+      }.bind(this));
+
       return this;
     };
 
